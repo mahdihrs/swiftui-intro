0ea5d63f07e8d64d88a24890feeabf222fa50b14
mahdihrs <mahdi.haris@myboost.id>
2020-10-21 08:48:26 +0700

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by adikminumsusu on 21/10/20.
+//  Copyright © 2020 self-org. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
